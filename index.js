e07778d0d784b3e12c0fcc2e41851e13588f6ae5
Varvara Molchanova <varvaram@compatibl.com>
2019-05-23 13:03:26 +0300

    delete unused stuff

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const {Client} = require('pg')
-const client = new Client({
-    user: "postgres",
-    password: "Pinokio33",
-    host: "localhost",
-    port: 5432,
-    database: "ipz1"
-})
-
-client.connect()
-.then(() => console.log("fuck yeah"))
-    .then()
-.catch(e => console.log)
-.finally(() => client.end())
-
 const express = require("express");
 
 const PORT = process.env.PORT || 5000
